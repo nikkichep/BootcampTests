bef49af01f62709272cd3dada8ac69321cb220dd
Nicole <124882220+nikkichep@users.noreply.github.com>
2024-07-30 08:59:31 +0000

    Checking the measurements a re equal to be a square if not its a rectangle

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ length = input("Enter the length")
 width = input("Enter the width")
 
 #Check if its a square when they are equal
+if length == width:
+  print("Its a square")
+else:
+  print("Its a rectangle")
